5ecd58e5bbadfd84ecb5868ed0bc64b1a3839eae
vikricahya64-alt <vikricahya64@gmail.com>
2026-08-29 21:45:27 +0700

    Delete agen/extensions directory

--- agen/extensions/ping.js
+++
@@ -1,8 +0,0 @@
-// extensions/ping.js
-function register(app) {
-  app.get('/ping-ekstensi', (req, res) => {
-    res.json({ status: 'ok', from: 'ekstensi', timestamp: new Date().toISOString() });
-  });
-  console.log('[Ping] Endpoint /ping-ekstensi terdaftar');
-}
-module.exports = { register };
