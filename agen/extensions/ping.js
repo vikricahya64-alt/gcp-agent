3e0c8caf8a1318486d7c8af1b8fdbe978f247c07
vikricahya64-alt <vikricahya64@gmail.com>
2026-08-29 21:32:18 +0700

    Change ping endpoint to ping-ekstensi

--- agen/extensions/ping.js
+++ agen/extensions/ping.js
@@ -1,8 +1,8 @@
 // extensions/ping.js
 function register(app) {
-  app.get('/ping', (req, res) => {
-    res.json({ status: 'ok', timestamp: new Date().toISOString(), message: 'Ekstensi berjalan!' });
+  app.get('/ping-ekstensi', (req, res) => {
+    res.json({ status: 'ok', from: 'ekstensi', timestamp: new Date().toISOString() });
   });
-  console.log('[Ping] Endpoint /ping terdaftar');
+  console.log('[Ping] Endpoint /ping-ekstensi terdaftar');
 }
 module.exports = { register };
